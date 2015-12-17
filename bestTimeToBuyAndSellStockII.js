cde0c381a735bf90f67afb3995c786d3925d5a2c
vickymin <minjiepei@gmail.com>
2015-12-17 10:25:20 +0800

    add best time to buy and sell stock II

---
+++ bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,18 @@
+//Say you have an array for which the ith element is the price of a given stock on day i.
+//Design an algorithm to find the maximum profit.
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var len = prices.length;
+    var max = 0;
+    for(var i = 0; i < len; i++){
+        var temp = prices[i];
+        var last = prices[i-1]
+        if(temp > last){
+            max = max + (temp - last);
+        }
+    }
+    return max;
+};
