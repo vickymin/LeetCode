ac3b9cb43a005cb4395ac3bc05b4bc7cab17c248
vickymin <minjiepei@gmail.com>
2015-12-20 10:25:14 +0800

    add contain duplicate

---
+++ containsDuplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort();
+    for(var i=1; i<nums.length; i++){
+        if(nums[i] == nums[i-1]){
+            return true;
+        }
+    }
+    return false;
+};
