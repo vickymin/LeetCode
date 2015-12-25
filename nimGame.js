99034efa859f5c6da085496c95c1c31aacb0087a
vickymin <minjiepei@gmail.com>
2015-12-25 14:19:54 +0800

    add nimGame

---
+++ nimGame.js
@@ -0,0 +1,25 @@
+//Given a binary tree, return the inorder traversal of its nodes' values.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    var arr = [];
+    _h(root, arr);
+    return arr;
+};
+var _h = function(root, arr){
+    if(root == null){
+        return;
+    }
+    _h(root.left, arr);
+    arr.push(root.val);
+    _h(root.right, arr);
+};
