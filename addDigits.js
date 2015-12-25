f1b856cbd60766a4971add20fc8e8d23e0455a45
vickymin <minjiepei@gmail.com>
2015-12-25 14:55:20 +0800

    add file

--- addDigits.js
+++ addDigits.js
@@ -1,9 +1,8 @@
-//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit
 /**
  * @param {number} num
  * @return {number}
  */
-
 var addDigits = function(num) {
     while (num >= 10) {
         var sum = 0;
--- majorityElement.js
+++ majorityElement.js
@@ -1,6 +1,6 @@
-//Given an array of size n, find the majority element.
-//The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-//You may assume that the array is non-empty and the majority element always exist in the array.
+//Given an array of size n, find the majority element
+//The majority element is the element that appears more than ⌊ n/2 ⌋ times
+//You may assume that the array is non-empty and the majority element always exist in the array
 /**
  * @param {number[]} nums
  * @return {number}
@@ -12,4 +12,21 @@ var majorityElement = function(nums) {
         return nums[0];
     }
     return nums[middle];
+};
+
+//O(n) answer
+var majorityElement = function(nums) {
+    var maj = nums[0];
+    var count = 1;
+    for(var i=1; i<nums.length; i++){
+        if(count === 0){
+            count++;
+            maj = nums[i];
+        }else if(maj == nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return maj;
 };
