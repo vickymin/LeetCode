8e8e1abc6049fd2f3dd357a6c9ebffdd1d186e58
vickymin <minjiepei@gmail.com>
2016-01-08 22:47:31 +0800

    add recursive

--- singleNumberIII.js
+++ singleNumberIII.js
@@ -15,3 +15,20 @@ var singleNumber = function(nums) {
     }
     return result;
 };
+//bit manipulation
+var singleNumber = function(nums) {
+    var diff = 0;
+    for(var i = 0; i < nums.length; i++){
+        diff ^= nums[i];
+    }
+    diff &= -diff;
+    var res = [0, 0];
+    for(var j = 0; j < nums.length; j++){
+        if((nums[j] & diff) == 0){
+            res[0] ^= nums[j];
+        }else{
+            res[1] ^= nums[j];
+        }
+    }
+    return res;
+};
