e8ef4e9dd01b81890cdd269a62e9aa24b2b4fb78
vickymin <minjiepei@gmail.com>
2016-01-10 23:12:58 +0800

    add file

---
+++ bulbSwitcher.js
@@ -0,0 +1,11 @@
+//There are n bulbs that are initially off. You first turn on all the bulbs.
+//Then, you turn off every second bulb. On the third round, you toggle every third bulb
+//(turning on if it's off or turning off if it's on).
+//For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+    return Math.floor(Math.sqrt(n));
+};
